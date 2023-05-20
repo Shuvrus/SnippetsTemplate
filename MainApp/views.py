46bc18bf28d00b7d0680672952d623ea7dc7c14a
Ruslan <shuvalov.r@mail.ru>
2023-05-20 23:30:45 +0300

    Change 20.05.23-1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -8,10 +10,66 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {
+        'form': form,
+        'pagename': 'Добавление нового сниппета'
+    }
     return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    quantity_snippets = len(snippets)
+    context = {'pagename': 'Просмотр сниппетов',
+               'snippets': snippets,
+               'quantity_snippets': quantity_snippets
+               }
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippet_detail(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    context = {
+        'pagename': 'Информация о сниппете',
+        'snippet': snippet
+    }
+    return render(request, 'pages/snippet_detail.html', context)
+
+
+def snippet_create(request):
+    if request.method == "POST":
+        # form_data = request.POST
+        # snippet = Snippet(
+        #     name=form_data['name'],
+        #     lang=form_data['lang'],
+        #     code=form_data['code'],
+        # )
+        # snippet.save()
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('snippets-list')
+
+
+def snippet_del(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    snippet.delete()
+    return redirect('snippets-list')
+
+
+def snippet_edit(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    context = {
+        'snippet': snippet,
+    }
+    return render(request, 'pages/edit_snippet.html', context)
+
+
+def snippet_change(request):
+    if request.method == 'POST':
+        form_data = request.POST
+        snippet = Snippet.objects.get(id=form_data['id'])
+        snippet.code = form_data['code']
+        snippet.save(update_fields=['code'])
+    return redirect('snippets-list')
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,6 +5,11 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page, name='home'),
-    path('snippets/add', views.add_snippet_page, name='snippet-add'),
-    path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippets/add/', views.add_snippet_page, name='snippet-add'),
+    path('snippets/create/', views.snippet_create, name='snippet-create'),
+    path('snippets/edit/<int:snippet_id>/', views.snippet_edit, name='snippet-edit'),
+    path('snippets/change', views.snippet_change, name='snippet-change'),
+    path('snippets/del/<int:snippet_id>/', views.snippet_del, name='snippet-del'),
+    path('snippets/list/', views.snippets_page, name='snippets-list'),
+    path('snippets/<int:snippet_id>/', views.snippet_detail, name='snippet-detail')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
